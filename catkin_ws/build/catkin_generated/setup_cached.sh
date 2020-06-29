7eb9b23dfacb92a77c736e676c3bbfdcd1db84a6
Samwoo Seong <tjdtkadn@gmail.com>
2020-06-29 16:15:40 -0700

    TF ROS practice
    TF ROS practice with Pi robot and turtlebot.

---
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export HUSKY_DESCRIPTION="/home/simulations/public_sim_ws/src/all/husky/husky/husky_description/urdf/description.xacro"
+export HUSKY_GAZEBO_DESCRIPTION="/home/simulations/public_sim_ws/src/all/husky/husky_simulator/husky_gazebo/urdf/description.gazebo.xacro"
+export TURTLEBOT_3D_SENSOR="asus_xtion_pro"
+export TURTLEBOT_BASE="kobuki"
+export TURTLEBOT_BATTERY="/sys/class/power_supply/BAT0"
+export TURTLEBOT_GAZEBO_MAP_FILE="/home/simulations/public_sim_ws/src/all/turtlebot/turtlebot_gazebo/maps/playground.yaml"
+export TURTLEBOT_GAZEBO_WORLD_FILE="/home/simulations/public_sim_ws/src/all/turtlebot/turtlebot_gazebo/worlds/playground.world"
+export TURTLEBOT_INTERACTIONS_LIST="[turtlebot_bringup/admin.interactions, turtlebot_bringup/documentation.interactions, turtlebot_bringup/pairing.interactions, turtlebot_bringup/visualisation.interactions]"
+export TURTLEBOT_NAME="turtlebot"
+export TURTLEBOT_RAPP_PACKAGE_BLACKLIST="[]"
+export TURTLEBOT_RAPP_PACKAGE_WHITELIST="[rocon_apps, turtlebot_rapps]"
+export TURTLEBOT_SERIAL_PORT="/dev/kobuki"
+export TURTLEBOT_SIMULATION="false"
+export TURTLEBOT_STACKS="hexagons"
+export TURTLEBOT_TYPE="turtlebot"
+
+# modified environment variables
+export GAZEBO_MODEL_PATH="/home/simulations/public_sim_ws/install/share/osrf_gear/models:$GAZEBO_MODEL_PATH"
+export LD_LIBRARY_PATH="/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/lib:/home/user/catkin_ws/devel/lib:/home/simulations/public_sim_ws/devel/lib:/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu:/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/lib:/usr/local/nvidia/lib:/usr/local/nvidia/lib64:/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64:/usr/lib/x86_64-linux-gnu/gazebo-7/plugins"
+export PATH="/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/bin:/home/simulations/public_sim_ws/devel/bin:/opt/ros/kinetic/bin:/usr/local/share/shell/.nvm/versions/node/v6.11.2/bin:/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/bin:/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+export PKG_CONFIG_PATH="/home/user/catkin_ws/devel/lib/pkgconfig:/home/simulations/public_sim_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD="/home/user/catkin_ws/build"
+export PYTHONPATH="/home/simulations/public_sim_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/user/catkin_ws/devel/share/common-lisp:/home/simulations/public_sim_ws/devel/share/common-lisp"
